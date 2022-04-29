059ac702dd81a18644b6e68c411659ee3ae1e309
JediStamp <jkmiklos@hotmail.com>
2022-04-29 11:20:32 -0400

    Complete to 3.07
    Completed exercises up to 03-07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,55 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const car = {
+  name: "Betsy",
+  make: "Nissan",
+  model: "Altima",
+  seatbelts: 4,
+  airbags: 5,
+  seatConfig: {
+    front: 2,
+    back: 3,
+  },
+  runStatus: false,
+  toggleRunStatus: function (runStatus) {
+    this.runStatus = runStatus;
+  },
+};
+
+const truck = {
+  name: "Sally",
+  make: "GMC",
+  model: "Sierra",
+  seatbelts: 6,
+  airbags: 7,
+  seatConfig: {
+    front: 3,
+    back: 3,
+  },
+  runStatus: true,
+  toggleRunStatus: function (runStatus) {
+    this.runStatus = runStatus;
+  },
+};
+
+console.log("Car: ", car);
+console.log("Truck:", truck);
+
+const laptop = {
+  type: "Asus",
+  ram: 16,
+  ramUnits: "GB",
+  case: "black",
+  peripherals: {
+    mouse: false,
+    keyboard: true,
+    monitor: {
+      monitor1: true,
+      monitor2: true,
+    },
+  },
+};
+
+console.log("Laptop:", laptop);
